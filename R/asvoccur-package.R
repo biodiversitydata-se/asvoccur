99c7a10b36980e54680838239f0b66f58e7a5714
Maria Prager <maria.prager@scilifelab.se>
2024-04-11 13:57:32 +0200

    Use roxygen to pull info from DESC. to pkg doc

--- R/asvoccur-package.R
+++ R/asvoccur-package.R
@@ -1,6 +1,10 @@
-#' @title Tools for ASV occurrence data in SBDI
-#' @name asvoccur
-#' @description This package provides functions for handling ASV occurrence data in SBDI.
+# Pull info from DESCRIPTION to asvoccur-package.Rd
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
 #' @importFrom data.table `:=`
 #' @importFrom data.table `.SD`
 #' @importFrom data.table data.table
@@ -9,10 +13,11 @@
 #' @importFrom data.table setkey
 #' @importFrom data.table setnames
 #' @importFrom stats setNames
+## usethis namespace: end
 NULL
 
-# To avoid `No visible binding` Notes from R CMD check.
-# Add any unquoted variables used by data.table functions 
+# To avoid `No visible binding` Notes from R CMD check,
+# add any unquoted variables used by data.table functions below
 # See 'non-standard evaluation (NSE)' & e.g.
 # https://www.r-bloggers.com/no-visible-binding-for-global-variable/
 .onLoad <- function(libname, pkgname){

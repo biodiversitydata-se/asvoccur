29875e1e95d2e9a16551bec93d5687575ddc7007
Maria Prager <maria.prager@scilifelab.se>
2024-05-23 09:50:03 +0200

    Update notes

--- notes/build_n_test_package.R
+++ notes/build_n_test_package.R
@@ -1,35 +1,33 @@
 # Some notes on how to build and test the package in RStudio
+# To keep local version of this file, use:
+# git update-index --assume-unchanged notes/build_n_test_package.R
+# Undo with:
+# git update-index --no-assume-unchanged notes/build_n_test_package.R
 
 # install.packages("devtools")
-# install.packages("roxygen2")
 
 # Load package into session for local testing
-setwd(dir = '/some-path-to/asvoccur')
 library(devtools)
 load_all(".")  # Alt. use ⇧ + ⌘ + L OR Build tab | More | Load All
 
-# Build help files:
+# Build help files with roxygen2:
 # See https://cran.r-project.org/web/packages/roxygen2/vignettes/roxygen2.html
-library(roxygen2)
-roxygenise()
-# Alt. set to automatically roxygenize when loading package
+# Alt 1: set to automatically roxygenize when loading package
 # Tools | Project Options | Build Tools | Generate doc. with roxygen
+# Alt. 2 (if project setting has no effect) use:
+# Build tab | More | Document
 
-# Test help files
+# View help files
+?asvoccur
 ?asvoccur::load_data
-help(asvoccur::load_data)
+?asvoccur::merge_data
+?asvoccur::sum_by_clade
 
 # Test functions
-data_path <- '/some-path-to/datasets'
+data_path <- '~/Downloads'
 loaded <- load_data(data_path)
-View(loaded$asv_tables[[1]])
-
+View(loaded$asvs[[1]])
 merged <- merge_data(loaded)
-View(merged$asv_table)
-
-
-
-
-
-
-
+View(merged$asvs)
+clade_sums <- sum_by_clade(merged$counts, merged$asvs)
+View(clade_sums$norm$class)
